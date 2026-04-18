fe9fff4ffd13b29b3623992d7cdbab7a15a250b8
Danny Dai <dannydai123@gmail.com>
2026-04-18 13:51:08 +0800

    A Small Quizzes
    A Small Quizzes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+  reactCompiler: false,
+  // webpack: (config, { dev, isServer }) => {
+  //   if (dev) {
+  //     // 'source-map' is slower but highly reliable for JetBrains
+  //     config.devtool = 'source-map';
+  //   }
+  //   return config;
+  // },
+
 };
 
 export default nextConfig;
